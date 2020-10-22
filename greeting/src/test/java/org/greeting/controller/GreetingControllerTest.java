4cc114484586aef825eabec6fd4465daab355546
mario.dacosta <mario-dacosta@hotmail.fr>
2020-10-22 11:27:23 +0200

    optimizing .gitlab-ci.yml

--- greeting/src/test/java/org/greeting/controller/GreetingControllerTest.java
+++ greeting/src/test/java/org/greeting/controller/GreetingControllerTest.java
@@ -1,23 +1,15 @@
 package org.greeting.controller;
 
-import org.greeting.model.Greeting;
 import org.greeting.service.GreetingService;
-import org.junit.Before;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.test.annotation.IfProfileValue;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.DisabledIf;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
-
 
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
